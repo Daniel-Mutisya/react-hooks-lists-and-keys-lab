873a74318f07accc4dba7e9af2a4c178eb33f334
daniel <danielmutisya203@gmail.com>
2023-04-28 01:23:52 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 function Home({ color, username, city }) {
   return (
     <div id="home">
+      
       <h1 style={{ color: color }}>
         {username} is a Web Developer from {city}
       </h1>
